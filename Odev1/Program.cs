251324c918327f40e316c6e27ca206d33ab7878a
kadcet <42303955+kadcet@users.noreply.github.com>
2022-11-10 00:48:22 +0300

    Add files via upload

---
+++ Odev1/Program.cs
@@ -0,0 +1,169 @@
+﻿
+#region 1.	Size göre en doğru döngüyü kullanarak ekranda 10 kez Merhaba yazan uygulamayı geliştirin.
+
+//Console.WriteLine("10 kere merhaba yazan uygulama");
+//byte yazdır = 1;
+//while (yazdır <= 10)
+//{
+//    Console.WriteLine("Merhaba");
+//    yazdır++;
+//}
+#endregion
+
+#region 2.	Kullanıcıdan alacağı değeri ekrana 5 kez yazdıran uygulamayı geliştirin.
+
+//Console.WriteLine("Gelen değeri 5 kere yazdıran uygulama");
+//Console.WriteLine("Değer Girin: ");
+//string gelenDeger = Console.ReadLine();
+//int yazdır1 = 1;
+//Console.Clear();
+//while (yazdır1 <= 5)
+//{
+//    Console.WriteLine(yazdır1 + "-" + gelenDeger);
+//    yazdır1++;
+//}
+#endregion
+
+
+#region 3.	0 – 10 aralığındaki (0 ve 10 dahil) tüm sayıların toplamını hesaplayıp ekrana yazan uygulamayı geliştirin
+//Console.WriteLine("0 – 10 aralığındaki (0 ve 10 dahil) tüm sayıların toplamını hesaplayıp ekrana yazan uygulama");
+//int a = 0;
+//for (int i = 1; i <= 10; i++)
+//{
+//    a = a + i;
+
+//}
+//Console.WriteLine("Toplam : " + a);
+#endregion
+
+
+
+#region 4.	0 dan başlayarak kullanıcıdan alacağınız sayıya kadar ki tüm sayıların toplamını bulan uygulamayı geliştirin
+//Console.WriteLine("0 dan başlayarak kullanıcıdan alacağınız sayıya kadar ki tüm sayıların toplamını bulan uygulama");
+//Console.WriteLine("Sayı giriniz : ");
+//int gelenDeger1 = Convert.ToInt32(Console.ReadLine());
+//Console.Clear();
+
+
+//for (int i = gelenDeger1; i > 0; i--)  // i >= 0; i--) olunca neden 14 çıkıyor. 0-1= -1 olduğu için mi
+//{
+//    gelenDeger1 = gelenDeger1 + (i - 1);
+//}
+//Console.WriteLine(gelenDeger1);
+#endregion
+
+
+#region 5.	Kullanıcıdan alınan sayının tek ya da çift olduğunu bulup ekrana “Tek” ya da “Çift” yazan uygulamayı geliştirin.
+
+//Console.WriteLine("Tek veya Çift Tam Sayı Giriniz");
+//int gelenDeger2 = Convert.ToInt32(Console.ReadLine());
+//if (gelenDeger2%2==0)
+//{
+//    Console.WriteLine($"{gelenDeger2} çift sayıdır");
+//}
+//else
+//{
+//    Console.WriteLine($"{gelenDeger2} tek sayıdır");
+//}
+
+#endregion
+
+
+#region 6.	Kullanıcıdan alınan sayıya kadar ki tek ve çift sayıların toplamını alarak ayrı ayrı ekrana yazan uygulamayı geliştirin.
+
+//Console.WriteLine("Sayı giriniz : ");
+//int gelenDeger3 = Convert.ToInt32(Console.ReadLine());
+//Console.Clear();
+
+
+//int ciftSayiToplam = 0;
+//int tekSayitoplam = 0;
+
+//for (int i = gelenDeger3; i >= 0; i--)
+//{
+//    if (i %2==0)
+//    {
+//        ciftSayiToplam = ciftSayiToplam + i;
+//        //ciftSayiToplam += i;
+//    }
+//    else
+//    {
+//        tekSayitoplam = tekSayitoplam + i;
+
+//    }
+//}
+
+//Console.WriteLine($"Çift sayılar toplamı {ciftSayiToplam}");
+//Console.WriteLine($"Tek sayılar toplamı {tekSayitoplam}");
+
+#endregion
+
+#region  7.	Kullanıcıdan 10 adet sayı alan ve bu sayıların 50 den küçük olanlarının TOPLAMINI bulup ekrana yazan uygulamayı geliştirin
+
+//int ellidenKucukToplam = 0;
+//int ellidenBuyukToplam = 0;
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine($"{i} .  sayıyı giriniz : ");
+//	int gelenDeger4 = Convert.ToInt32(Console.ReadLine());
+//	if (gelenDeger4<50)
+//	{
+//		ellidenKucukToplam += gelenDeger4;
+//	}
+//	else
+//	{
+//		ellidenBuyukToplam += gelenDeger4;
+//	}
+
+//}
+//Console.Clear();
+//Console.WriteLine($" 50 den küçük sayılar toplamı : {ellidenKucukToplam}");
+//Console.WriteLine($" 50 den büyük sayılar toplamı : {ellidenBuyukToplam}");
+
+#endregion
+
+#region  7.	Kullanıcıdan 10 adet sayı alan ve bu sayıların 50 den küçük olanlarının SAYISINI bulup ekrana yazan uygulamayı geliştirin
+
+//int ellidenKucuk = 0;
+//int ellidenBuyuk = 0;
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine($"{i} .  sayıyı giriniz : ");
+//    int gelenDeger4 = Convert.ToInt32(Console.ReadLine());
+//    if (gelenDeger4 < 50)
+//    {
+//        ellidenKucuk++;
+//    }
+//    else
+//    {
+//        ellidenBuyuk++;
+//    }
+
+//}
+//Console.Clear();
+//Console.WriteLine($" 50 den küçük sayılar adedi : {ellidenKucuk}");
+//Console.WriteLine($" 50 den büyük sayılar adedi : {ellidenBuyuk}");
+
+#endregion
+
+#region 8.	Kullanıcıdan kaç tane sayıyı toplamak istediğini alan sonrasında toplanacak sayı adedi kadar sayıyı kullanıcıdan tek tek alıp bu sayıların toplamını ekrana yazan programı geliştirin
+
+Console.WriteLine("Kaç Sayı toplamak istersiniz : ");
+int kacSayi = Convert.ToInt32(Console.ReadLine());
+int toplamGelen = 0;
+Console.Clear();
+for (int i = 1; i <= kacSayi; i++)
+{
+    Console.WriteLine($"{i} . sayıyı girin");
+    int toplamGelen1=Convert.ToInt32(Console.ReadLine());
+    toplamGelen += toplamGelen1;
+
+}
+Console.WriteLine($"Toplam gelen sayı toplamı : {toplamGelen}");
+
+
+#endregion
+
+
+
+
---
+++ Odev2/Program.cs
@@ -0,0 +1,97 @@
+﻿
+
+using static System.Net.Mime.MediaTypeNames;
+
+Console.Title = "Hesap Makinesi V-1.0";
+basaDon:;
+Console.WriteLine("1. Toplama");
+Console.WriteLine("2. Çıkarma");
+Console.WriteLine("3. Çarpma");
+Console.WriteLine("4. Bölme");
+Console.WriteLine("5. Çıkış");
+Console.WriteLine("Lütfen seçim yapınız :");
+
+int gelenDeger = Convert.ToInt32(Console.ReadLine());
+
+
+switch (gelenDeger)
+{
+    case 1:
+        Console.Clear();
+        Console.WriteLine("1. operantı giriniz :");
+        int gelenToplamaOperant1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("2. operantı giriniz :");
+        int gelenToplamaOperant2 = Convert.ToInt32(Console.ReadLine());
+        var result = gelenToplamaOperant1 + gelenToplamaOperant2;
+        Console.WriteLine($"{gelenToplamaOperant1} + {gelenToplamaOperant2} = {result}");
+        basa();
+        goto basaDon;
+
+        break;
+
+    case 2:
+        Console.Clear();
+        Console.WriteLine("1. operantı giriniz :");
+        int gelenCikarmaOperant1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("2. operantı giriniz :");
+        int gelenCikarmaOperant2 = Convert.ToInt32(Console.ReadLine());
+        result = gelenCikarmaOperant1 - gelenCikarmaOperant2;
+        Console.WriteLine($"{gelenCikarmaOperant1} - {gelenCikarmaOperant2} = {result}");
+        basa();
+        goto basaDon;
+        break;
+
+    case 3:
+        Console.Clear();
+        Console.WriteLine("1. operantı giriniz :");
+        int gelenCarpmaOperant1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("2. operantı giriniz :");
+        int gelenCarpmaOperant2 = Convert.ToInt32(Console.ReadLine());
+        result = gelenCarpmaOperant1 * gelenCarpmaOperant2;
+        Console.WriteLine($"{gelenCarpmaOperant1} * {gelenCarpmaOperant2} = {result}");
+        basa();
+        goto basaDon;
+        break;
+    case 4:
+        Console.Clear();
+        Console.WriteLine("1. operantı giriniz :");
+        decimal gelenBolmeOperant1 = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("2. operantı giriniz :");
+        decimal gelenBolmeOperant2 = Convert.ToDecimal(Console.ReadLine());
+        decimal decimalResult = gelenBolmeOperant1 / gelenBolmeOperant2;
+        Console.WriteLine($"{gelenBolmeOperant1} / {gelenBolmeOperant2} = {decimalResult}");
+        basa();
+        goto basaDon;
+        break;
+    case 5:
+        Environment.Exit(0);
+        break;
+
+    default:
+        Console.Clear();
+        Console.WriteLine("Hatalı seçim yaptınız");
+        goto basaDon;
+        break;
+
+
+}
+
+
+
+void basa()
+{
+    Console.WriteLine("Yeni bir işlem yapmak ister misiniz? (e/h)");
+    string gelenCevap = Console.ReadLine();
+    if (gelenCevap == "e" || gelenCevap == "E")
+    {
+        Console.Clear();
+        
+    }
+    else if (gelenCevap=="h" || gelenCevap=="H")
+    {
+        Environment.Exit(0);
+    }
+}
+
+
+
